3ae0fdbbfd53e7f068221ad960ef382576355356
EuDs63 <erica23187@gmail.com>
2023-08-29 10:20:47 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# 
+# BookRecommend_Back
 
 **此处存放后端项目**
+
+## 配置
+1. 修改config.py.example中的内容
+2. 将config.py.example重命名为config.py
--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def ping():
     return "OK"
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5002, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
