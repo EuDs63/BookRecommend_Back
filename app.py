fffa976e3b9049ae6dc74f9274a92fd503dc14bd
EuDs63 <erica23187@gmail.com>
2023-08-30 11:07:41 +0800

    支持前后端在同一个局域网中通信

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ def ping():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    #app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
