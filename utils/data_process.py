977fa80eb8ae5e0fa9f3fe6be3a56f81e616fce6
EuDs63 <erica23187@gmail.com>
2023-09-01 10:32:14 +0800

    规范命名

--- utils/data_process.py
+++ utils/data_process.py
@@ -1,7 +1,7 @@
 # 目的： database data  =======>  dict data/array data
 
-def Data_Process(data, fields, type=0): # （数据源，哪些字段，数组0/对象1）
-    if not type:  # data: [obj,obj,obj,obj,obj,...]
+def Data_Process(data, fields, data_type=0):  # （数据源，哪些字段，数组0/对象1）
+    if not data_type:  # data: [obj,obj,obj,obj,obj,...]
         # 声明新 空 数组
         result = []
         for u in data:
